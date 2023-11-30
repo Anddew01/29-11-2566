e6c043b84d297009f9d7519e4a10c77b633aac80
Anddew01 <149749051+Anddew01@users.noreply.github.com>
2023-11-30 18:47:57 +0700

    Delete IF,ELSE IF,ELSE directory

--- IF,ELSE IF,ELSE/Lab09.js
+++
@@ -1,8 +0,0 @@
-/**9. Temperature Convert
-ให้เขียนฟังก์ชันแปลงอุณหภูมิจาก farenheit เป็น celsius
- */
-const fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * (5/9);
-const F = 77.0;
-const C = fahrenheitToCelsius(F);
-
-console.log(`${F} องศาฟาเรนไฮต์ เท่ากับ ${C.toFixed(2)} องศาเซลเซียส`);
--- IF,ELSE IF,ELSE/Lab13.js
+++
@@ -1,4 +0,0 @@
-/**โค้ดด้านล่างทำงานได้หรือไม */
-let company = prompt('Which company created JavaScript?');
-company == 'Netscape' ? alert('Right!') : alert('Wrong.');
-//ทำงานได้
